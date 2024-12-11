5d01ac03e0ab85adce91536bb4c91e799b9eb1e3
Colby Domingos <colbydomingos.ccd@gmail.com>
2024-12-10 18:57:31 -0800

    Add project files.

---
+++ simpleCalculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleCalculator
+{
+    internal class Program
+    {
+        static void start()
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            start();
+        }
+    }
+}
