81a68a3b0485d0e629b6ddfd396bd80b9591803f
Colby Domingos <colbydomingos.ccd@gmail.com>
2024-12-10 19:32:46 -0800

    added basic functionality

--- simpleCalculator/Program.cs
+++ simpleCalculator/Program.cs
@@ -8,15 +8,72 @@ namespace simpleCalculator
 {
     internal class Program
     {
-        static void start()
+        static void startMessage()
         {
+            Console.WriteLine("|===========================================|");
+            Console.WriteLine("What type of calculation would you like to do?");
+            Console.WriteLine("                 Version 1.0");
+            Console.WriteLine("|===========================================|");
+
+            Console.WriteLine("-> Addition (+)");
+            Console.WriteLine("-> Subtraction (-)");
+            Console.WriteLine("-> Multiplication (*)");
+            Console.WriteLine("-> Division (/)");
+            Console.WriteLine("-> Remainder (%)");
+        }
+
+        static void mathCalc()
+        {
+            double num3 = 0;
+
+            string op = Console.ReadLine();
+            char operation = Convert.ToChar(op);
+
+            Console.WriteLine("Enter your first number: ");
+            Console.WriteLine("");
+            string number1 = Console.ReadLine();
+            double num1 = Convert.ToDouble(number1);
+            
+            Console.WriteLine("Enter your second number: ");
+            Console.WriteLine("");
+            string number2 = Console.ReadLine();
+            double num2 = Convert.ToDouble(number2);
+
+            Console.WriteLine(num1 + " " + operation + " " + num2);
+
+            switch (operation) 
+            {
+                case '+':
+                    num3 = num1 + num2;
+                    Console.WriteLine("Your answer is: " + num3);
+                    break;
+                case '-':
+                    num3 = num1 - num2;
+                    Console.WriteLine("Your answer is: " + num3);
+                    break;
+                case '*':
+                    num3 = num1 * num2;
+                    Console.WriteLine("Your answer is: " + num3);
+                    break;
+                case '/':
+                    num3 = num1 / num2;
+                    Console.WriteLine("Your answer is: " + num3);
+                    break;
+                case '%':
+                    num3 = num1 % num2;
+                    Console.WriteLine("Your answer is: " + num3);
+                    break;
+            }
+
 
         }
 
 
         static void Main(string[] args)
         {
-            start();
+            startMessage();
+
+            mathCalc();
         }
     }
 }
